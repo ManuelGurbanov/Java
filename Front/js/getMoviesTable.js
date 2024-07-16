6c23733878186c27c469e9bc7583f6f4e43c5a80
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-07-15 21:29:51 -0300

    added all movies table

--- Front/js/getMoviesTable.js
+++ Front/js/getMoviesTable.js
@@ -1,14 +1,20 @@
 fetch('http://localhost:8080/app/pelicula')
 .then(response => response.json())
 .then(data => {
-  const moviesTable = document.getElementById('moviesTable');
+  const moviesTableBody = document.querySelector('#moviesTable tbody');
 
   data.forEach(movie => {
-    const movieRow = document.createElement('th');
+    const movieRow = document.createElement('tr');
     movieRow.classList.add('movieRow');
 
-    movieRow.innerHTML = '<tr> <th scope="row">' + movie.id + '</th> <td>' + movie.titulo + '</td> <td>' + movie.genero + '</td> <td>' + movie.duracion + '</td> <td>' + movie.imagen + '</td> </tr>';
-    moviesTable.appendChild(movieRow);
+    movieRow.innerHTML = `
+      <th scope="row">${movie.id}</th>
+      <td>${movie.titulo}</td>
+      <td>${movie.genero}</td>
+      <td>${movie.duracion}</td>
+      <td><img src="${movie.imagen}" alt="${movie.titulo}" class="movie-image"></td>
+    `;
+    moviesTableBody.appendChild(movieRow);
   });
 })
 .catch(error => console.error('Error fetching movies:', error));
